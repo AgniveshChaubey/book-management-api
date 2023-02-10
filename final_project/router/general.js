1e5f71f958949f4f28c54e9ac32a6ff8539f6b70
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-02-10 23:44:41 +0530

    authentication mechenism set up

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,6 +13,7 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
+  res.send(JSON.stringify(books));
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
@@ -21,7 +22,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
  });
-  
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
