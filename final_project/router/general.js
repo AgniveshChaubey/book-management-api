74ed8172e0e7d33cde10fc1f8b16767cf2b1b8a2
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-06-16 21:11:03 +0530

    task 5 completed - Get book Review based on isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -50,8 +50,12 @@ public_users.get("/title/:title", function (req, res) {
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  if (books.hasOwnProperty(isbn)) {
+    res.json(books[isbn].reviews);
+  } else {
+    res.status(404).json({ error: "Book not found" });
+  }
 });
 
 module.exports.general = public_users;
