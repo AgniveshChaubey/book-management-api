dee791d8e0aa53abbd3fd96fd5ca514ed42d2295
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-06-16 19:10:06 +0530

    task 3 completed - Get all books by Author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -14,7 +14,6 @@ public_users.post("/register", (req, res) => {
 // Get the book list available in the shop
 public_users.get("/", function (req, res) {
   res.json(books);
-  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get book details based on ISBN
@@ -25,14 +24,17 @@ public_users.get("/isbn/:isbn", function (req, res) {
   } else {
     res.status(404).json({ error: 'Book not found' });
   }
-  console.Console.log(isbn)
-  return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const author = req.params.author;
+  const book = Object.values(books).find(book => book.author === author);
+  if(book) {
+    res.json(book)
+  } else {
+    res.status(404).json({ error: "Book not found!" });
+  }
 });
 
 // Get all books based on title
