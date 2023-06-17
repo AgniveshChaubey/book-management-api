db0e7ac9d3326cf85d091169736c34db3ab68f93
Agnivesh Chaubey <agniveshvapi@gmail.com>
2023-06-17 11:22:23 +0530

    task 11 completed - Search by ISBN – Using Promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -8,8 +8,8 @@ public_users.post("/register", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
   if (isValid(username)) {
-    users.push({username, password});
-    res.json({"message" : "Registered Successfully!", "user" : username })
+    users.push({ username, password });
+    res.json({ message: "Registered Successfully!", user: username });
   } else {
     res.send("User already exist!");
   }
@@ -17,22 +17,51 @@ public_users.post("/register", (req, res) => {
 
 public_users.get("/books", (req, res) => {
   res.send(books);
-})
+});
 
 // Get the book list available in the shop
 public_users.get("/", async function (req, res) {
   res.json(books);
 });
 
-// Get book details based on ISBN
+// // Get book details based on ISBN
+// public_users.get("/isbn/:isbn", function (req, res) {
+//   const isbn = req.params.isbn;
+//   if (books.hasOwnProperty(isbn)) {
+//     res.json(books[isbn]);
+//   } else {
+//     res.status(404).json({ error: "Book not found" });
+//   }
+// });
+
+// Get book details based on ISBN (Using Promises)
 public_users.get("/isbn/:isbn", function (req, res) {
   const isbn = req.params.isbn;
-  if (books.hasOwnProperty(isbn)) {
-    res.json(books[isbn]);
-  } else {
-    res.status(404).json({ error: "Book not found" });
-  }
+  searchBookByISBN(isbn)
+    .then((book) => {
+      if (book) {
+        res.json(book);
+      } else {
+        res.status(404).json({ error: "Book not found" });
+      }
+    })
+    .catch((error) => {
+      console.error("Error searching book by ISBN:", error);
+      res.status(500).json({ error: "Internal server error" });
+    });
 });
+
+function searchBookByISBN(isbn) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (books.hasOwnProperty(isbn)) {
+        resolve(books[isbn]);
+      } else {
+        resolve(null);
+      }
+    }, 1000);
+  });
+}
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
